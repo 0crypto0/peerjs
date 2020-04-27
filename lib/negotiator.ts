4122629af8c00a9949b866ec108039cd7f35bc91
ori <ori@spectory.com>
2020-04-27 17:11:05 +0300

    update

--- lib/negotiator.ts
+++ lib/negotiator.ts
@@ -110,10 +110,10 @@ export class Negotiator {
             "iceConnectionState is disconnected, closing connections to " +
             peerId
           );
-          // this.connection.emit(
-          //   ConnectionEventType.Error,
-          //   new Error("Connection to " + peerId + " disconnected.")
-          // );
+          this.connection.emit(
+            ConnectionEventType.Error,
+            new Error("Connection to " + peerId + " disconnected.")
+          );
           // this.connection.close();
           console.warn("Connection to " + peerId + " disconnected.")
           break;
